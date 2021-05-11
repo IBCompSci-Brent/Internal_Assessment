23f75ef7106e8a9a2fee2dc7d651b1b79d1def69
IBCompSci-Brent <brent.tai@britishschoolmanila.org>
2021-05-11 11:57:25 +0800

    -

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import sqlite3
 from tkinter import *
 from tkinter import messagebox
 
-root = Tk()
-root.geometry("500x500")
-root.title('Company Database')
-
 conn = sqlite3.connect('company.db')
 c = conn.cursor()
 
+
+root = Tk()
 
 
 
