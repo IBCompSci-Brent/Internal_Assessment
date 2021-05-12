4e3d2a2eb27f3a0da0e921a10e6a4d6db51e77c2
IBCompSci-Brent <brent.tai@britishschoolmanila.org>
2021-05-12 23:36:25 +0800

    -

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from tkinter import messagebox as mb
 
 LARGE_FONT = ("Verdana", 12)
 root = tk.Tk()
+root.title('Company Database')
+root.geometry('500x500')
 container = tk.Frame(root)
+
+conn = sqlite3.connect('company.db')
+c = conn.cursor()
 
 container.pack(side="top", fill="both", expand=True)
 
@@ -26,11 +31,40 @@ def show_frame(frame_to_raise):
 
 # Code for first
 
+# Code for add_department
+def enter():
+    conn = sqlite3.connect('company.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO departments VALUES (:DepartmentID, :Name)",
+            {
+                'DepartmentID': DepartmentID.get(),
+                'Name': Name.get()
+            })
+    conn.commit()
+    conn.close()
+    DepartmentID.delete(0, END)
+    Name.delete(0, END)
+
+DepartmentID = Entry(root, width=30)
+DepartmentID.grid(row=0, column=1, padx=20)
+Name = Entry(root, width=30)
+Name.grid(row=1, column=1)
+
+DepartmentID_label = Label(root, text="Department ID")
+DepartmentID_label.grid(row=0, column=0)
+Name_label = Label(root, text="Name")
+Name_label.grid(row=1, column=0)
+
+enter_btn = Button(root, text="Add Department To Database", command=enter)
+enter_btn.grid(row=2, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
 
 
 
 
 
+
+conn.commit()
+conn.close()
 show_frame(first)
 root.mainloop()
 
