6ec7f1a18baec2203053e534ae3a8545da2d1ea6
IBCompSci-Brent <brent.tai@britishschoolmanila.org>
2021-12-16 21:23:28 +0800

    -

--- company.py
+++ company.py
@@ -23,7 +23,7 @@ c.execute("""CREATE TABLE IF NOT EXISTS employees (
         Account text,
         Account_Number integer,
         Social_Security_Number integer,
-        Joined integer,
+        Joined text,
         Salary integer,
         EmployeeID INTEGER PRIMARY KEY,
         DepartmentID integer,
@@ -50,10 +50,10 @@ c.execute("INSERT INTO departments VALUES ('Human Resources',null)")
 c.execute("INSERT INTO departments VALUES ('Sales',null)")
 c.execute("INSERT INTO departments VALUES ('Research',null)")
 
-c.execute("INSERT INTO employees VALUES ('James','06-04-04','Male','Finance','CEO','09173236403','james@gmail.com','RCBC','100000000000','1000000000','010121','100',null,1)")
-c.execute("INSERT INTO employees VALUES ('John','06-05-04','Male','Human Resources','Manager','09173236404','john@gmail.com','HSBC','200000000000','2000000000','010221','200',null,2)")
-c.execute("INSERT INTO employees VALUES ('Chris','06-06-04','Female','Sales','Employee','09173236405','chris@gmail.com','BDO','300000000000','3000000000','010321','300',null,3)")
-c.execute("INSERT INTO employees VALUES ('Sam','06-07-04','Male','Research','COO','09173236406','sam@gmail.com','ICBC','400000000000','4000000000','010421','400',null,4)")
+c.execute("INSERT INTO employees VALUES ('James','06-04-04','Male','Finance','CEO','09173236403','james@gmail.com','RCBC','100000000000','1000000000','01-01-21','100',null,1)")
+c.execute("INSERT INTO employees VALUES ('John','06-05-04','Male','Human Resources','Manager','09173236404','john@gmail.com','HSBC','200000000000','2000000000','01-02-21','200',null,2)")
+c.execute("INSERT INTO employees VALUES ('Chris','06-06-04','Female','Sales','Employee','09173236405','chris@gmail.com','BDO','300000000000','3000000000','01-03-21','300',null,3)")
+c.execute("INSERT INTO employees VALUES ('Sam','06-07-04','Male','Research','COO','09173236406','sam@gmail.com','ICBC','400000000000','4000000000','01-04-21','400',null,4)")
 
 c.execute("INSERT INTO records VALUES ('James','03','01','21','9','0','17','0',null,1)")
 c.execute("INSERT INTO records VALUES ('John','03','02','21','9','30','17','30',null,2)")
